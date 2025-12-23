8a1e52d8bff4b1bee17d4b1408aead2dcac27ded
robbymilo <robbymilo@fastmail.com>
2025-12-23 19:08:36 +0100

    add tests

--- pkg/exif/exif_test.go
+++ pkg/exif/exif_test.go
@@ -19,6 +19,8 @@ func TestStringToDate(t *testing.T) {
 	time_strings["20190323-wedding.jpg"] = "2019-03-23 00:00:00Z"
 	time_strings["20190323-1.jpg"] = "2019-03-23 00:00:00Z"
 	time_strings["20190323-2.jpg"] = "2019-03-23 00:00:00Z"
+	time_strings["20190323_wedding-03.jpg"] = "2019-03-23 00:00:00Z"
+	time_strings["20190323-wedding_03.jpg"] = "2019-03-23 00:00:00Z"
 	time_strings["20190323_wedding_03.jpg"] = "2019-03-23 00:00:00Z"
 	time_strings["20190323_wedding.jpg"] = "2019-03-23 00:00:00Z"
 	time_strings["20190323_1.jpg"] = "2019-03-23 00:00:00Z"
